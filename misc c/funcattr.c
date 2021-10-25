92331bf84d0ae66fd9d300a4361bfcff16dae009
Anindya <anindya20024@iiitd.ac.in>
2021-10-25 17:26:36 +0530

    refractored

--- misc c/funcattr.c
+++
@@ -1,9 +0,0 @@
-int square(int num, int a, int b, int c, int d, int e, int f, int g, int h) {
-    return num*a*b*c*d*e*f*g*h;
-}
-
-int main(){
-    int a = square(10, 20, 30, 40, 50, 60, 70, 80, 90);
-    a = 3;
-    return 0;
-}
--- misc c/onlyfunc.c
+++
@@ -1,8 +0,0 @@
-// Type your code here, or load an example.
-int square(int num) {
-    return num*num;
-}
-
-int main(){
-    int a = square(10);
-}
